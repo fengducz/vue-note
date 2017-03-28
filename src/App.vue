7d106e6f725555f1f0d3c0efbf8b2060a164ff46
fengducz <shangzq00@163.com>
2017-03-28 11:34:59 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+<div id="app">
+    <div class="container-fluid">
+        <div class="row">
+            <Toolbar></Toolbar>
+            <NotesList></NotesList>
+            <Editor></Editor>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+import store from './vuex/store'
+import Toolbar from './components/ToolBar.vue';
+import NotesList from './components/NotesList.vue';
+import Editor from './components/Editor.vue';
+export default {
+    name: 'app',
+    store,
+    components: {
+        Toolbar,
+        NotesList,
+        Editor
+    }
+}
+</script>
+
+<style>
+html,
+body,
+.container-fluid,
+.row,
+#app {
+    height: 100%;
+}
+</style>
---
+++ src/components/Toolbar.vue
@@ -0,0 +1,53 @@
+<template  >
+<div class="col-md-1" id="tool-bar">
+    <i class="glyphicon glyphicon-list-alt"></i>
+    <i class="glyphicon glyphicon-plus" @click="newNote"></i>
+    <i class="glyphicon glyphicon-star-empty " :class="{starred: activeNote.favorite}" @click="favoriteNote"></i>
+    <i class="glyphicon glyphicon-remove" @click="delNote"></i>
+</div>
+</template>
+
+<script>
+import {
+    mapGetters,
+    mapActions
+} from 'vuex'
+export default {
+    computed: {
+        ...mapGetters([
+              'activeNote'
+    ])
+  },
+    methods: {
+        ...mapActions([
+                "newNote",
+                'delNote',
+                'favoriteNote'
+            ]
+        )
+}
+}
+</script>
+
+<style lang="css">
+#tool-bar {
+    width: 80px;
+    height: 100%;
+    background-color: #30414D;
+    color: #767676;
+    padding: 35px 25px 25px 25px;
+}
+i {
+    font-size: 30px;
+    margin-bottom: 35px;
+    cursor: pointer;
+    opacity: 0.8;
+    transition: opacity 0.5s ease;
+}
+.starred {
+    color: #F7AE4F;
+}
+i:hover {
+    opacity: 1;
+}
+</style>
